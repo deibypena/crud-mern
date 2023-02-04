d1b2618fb42cf90ad0b4c2b7a6b66c1317900541
deibypena <ejmovin@gmail.com>
2023-02-04 10:58:17 -0500

    configuracion para deloy

--- server/config.js
+++ server/config.js
@@ -1,1 +1,7 @@
 export const PORT = process.env.PORT || 4000;
+
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_USER = process.env.DB_USER || 'prueba';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'prueba';
+export const DB_NAME = process.env.DB_NAME || 'sistema-inventario';
+export const DB_PORT = process.env.DB_PORT || 3306;
--- server/db.js
+++ server/db.js
@@ -1,8 +1,10 @@
 import { createPool } from 'mysql2/promise';
+import { DB_HOST, DB_PASSWORD, DB_NAME, DB_PORT, DB_USER } from './config.js'
 
 export const pool = new createPool({
-    host: 'localhost',
-    user: 'prueba',
-    password: 'prueba',
-    database: 'sistema-inventario'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT,
 })
