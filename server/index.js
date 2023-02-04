c4a494f3eb4cb99f06466531a8430715b886f022
deibypena <ejmovin@gmail.com>
2023-02-04 11:37:52 -0500

    prueba config deploy

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import {dirname, join} from 'path';
 import {fileURLToPath} from 'url';
 import { PORT } from './config.js';
-import indexRoutes from './routes/index.routes.js';
 import taskRoutes from './routes/tasks.routes.js';
 
 const app = express();
@@ -12,7 +11,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(cors());
 app.use(express.json());
 
-app.use(indexRoutes);
 app.use(taskRoutes);
 
 app.use(express.static(join(__dirname, '../client/dist')));
