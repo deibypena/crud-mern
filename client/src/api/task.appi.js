e2b19de8f13bbc0c7580e2b85a8a5bd76b8a98e4
deibypena <ejmovin@gmail.com>
2023-02-04 11:22:30 -0500

    prueba url

--- client/src/api/task.appi.js
+++ client/src/api/task.appi.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const getTasksRequest = async () => await axios.get('http://localhost:4000/tasks')
+export const getTasksRequest = async () => await axios.get('https://sistema-inventario-production.up.railway.app//tasks')
 
-export const createTaskRequest = async (task) => await axios.post('http://localhost:4000/tasks', task)
+export const createTaskRequest = async (task) => await axios.post('https://sistema-inventario-production.up.railway.app/tasks', task)
 
-export const deleteTaskRequest = async (id) => await axios.delete(`http://localhost:4000/tasks/${id}`)
+export const deleteTaskRequest = async (id) => await axios.delete(`https://sistema-inventario-production.up.railway.app/tasks/${id}`)
 
-export const getTaskRequest = async (id) => await axios.get(`http://localhost:4000/tasks/${id}`)
+export const getTaskRequest = async (id) => await axios.get(`https://sistema-inventario-production.up.railway.app/tasks/${id}`)
 
-export const updateTaskRequest = async (id, newFields) => await axios.put(`http://localhost:4000/tasks/${id}`, newFields)
+export const updateTaskRequest = async (id, newFields) => await axios.put(`https://sistema-inventario-production.up.railway.app/tasks/${id}`, newFields)
