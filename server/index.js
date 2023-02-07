4eb08e012b731995962a4b83df8e07f549879c7d
deibypena <ejmovin@gmail.com>
2023-02-07 15:53:35 -0500

    ASD

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import {dirname, join, path} from 'path';
+import {dirname, join} from 'path';
 import {fileURLToPath} from 'url';
 import { PORT } from './config.js';
 import taskRoutes from './routes/tasks.routes.js';
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(cors());
 app.use(express.json());
@@ -15,14 +14,6 @@ app.use(express.json());
 app.use(taskRoutes);
 
 app.use(express.static(join(__dirname, '../client/dist')));
-
-app.get('*', (req, res) => {    
-    res.sendFile(path.join(publicPath, 'index.html')), function(err) {             
-    if (err) {                 
-         res.status(500).send(err) 
-         }        
-    };
-});
 
 app.listen(PORT)
 console.log(`se esta escuchando en el puerto ${PORT}`);
