b93df2a180ae9762d6f8a9f1e1dd6bc90d44a7a6
deibypena <ejmovin@gmail.com>
2023-02-08 08:36:08 -0500

    asdasd

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,20 +8,19 @@ function Login() {
   return (
     <div>
       <Formik
-        initialValues={task}
         enableReinitialize={true}
         onSubmit={ async (values, actions) => {
           await createUser(values)
           navigate("/")
         }}
       >
-      {({handleChange, handleSubmit, values, isSubmitting}) => (
+      {({handleChange, handleSubmit, isSubmitting}) => (
         <Form onSubmit={handleSubmit} className="bg-slate-100 rounded-md p-6 w-full max-w-xl flex flex-col mx-auto">
           <label className='text-xl font-semibold mb-2'>Title</label>
-          <input className='rounded-md p-2 border border-zinc-400 mb-6' type="text" name='title' placeholder='aqui va el titulo' value={values.title} onChange={handleChange} />
+          <input className='rounded-md p-2 border border-zinc-400 mb-6' type="text" name='title' placeholder='aqui va el titulo' />
 
           <label className='text-xl font-semibold mb-2'>Password</label>
-          <input className='rounded-md p-2 border border-zinc-400 mb-6' type="password" name='password' placeholder='aqui va la contraseña' value={values.password} onChange={handleChange} />
+          <input className='rounded-md p-2 border border-zinc-400 mb-6' type="password" name='password' placeholder='aqui va la contraseña'/>
           
           <button className='bg-slate-500 text-white font-bold border border-zinc-400 rounded-md py-2 px-4 w-fit mx-auto' type="submit" disabled={isSubmitting} >{isSubmitting ? "saving..." : "Save"}</button>
         </Form>
